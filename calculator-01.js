7a26ad2600ae106927d206df9b74a4c3903de0eb
Artyom Harutyunyan <ka6rias@gmail.com>
2023-09-02 17:02:38 +0400

    task 1 rewrited

--- calculator-01.js
+++ calculator-01.js
@@ -6,24 +6,21 @@ Multiply two numbers.
 Divide two numbers.
 */
 function Calculator() {
-  this.add = function (num1, num2) {
-    return num1 + num2;
-  };
-  this.subtract = function (num1, num2) {
-    return num1 - num2;
-  };
-  this.multiply = function (num1, num2) {
-    return num1 * num2;
-  };
-  this.divide = function (num1, num2) {
-    return num1 / num2;
-  };
+
 }
-let calc=new Calculator();
-console.log(calc.add(1,2));
-console.log(calc.subtract(1,2));
-console.log(calc.multiply(1,2));
-console.log(calc.divide(1,2));
+
+Calculator.prototype.add = function (num1, num2) {
+  return num1 + num2;
+}
+Calculator.prototype.substract = function (num1, num2) {
+  return num1 - num2
+}
+Calculator.prototype.multiply = function (num1, num2) {
+  return num1 * num2
+}
+Calculator.prototype.divide = function (num1, num2) {
+  return num1 / num2
+}
 
 
 
